e6e03d579316fd91a7b546a8e3df087603720b1b
LHuang2019 <lhuang210@gmail.com>
2016-12-21 20:51:39 -0500

    Create and comment main class

--- src/spojProblems/SolutionWindow.java
+++ src/spojProblems/SolutionWindow.java
@@ -1,5 +1,20 @@
 package spojProblems;
 
+/**
+ * Class which solves the given problem
+ * @author Li Huang
+ * @ version 12.21.2016
+ */
 public class SolutionWindow {
 
+    /**
+     * The main method
+     * @param args
+     * @throws java.lang.Exception
+     */
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        
+    }
+    
 }
