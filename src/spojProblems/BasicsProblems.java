97bcc58c732e47dd00a5ae53c8972d87baaabe13
LHuang2019 <lhuang210@gmail.com>
2016-12-21 21:15:35 -0500

    Answered Test 1 for learning how to work on Spoj

--- src/spojProblems/BasicsProblems.java
+++ src/spojProblems/BasicsProblems.java
@@ -1,10 +1,23 @@
 package spojProblems;
 
+import java.util.Scanner;
+
 /**
  * Class which has the solution for basic problems
  * @author Li Huang
  * @version 12.21.2016
  */
 public class BasicsProblems {
+    
+    /**
+     * Method for Test 1, written for reference
+     */
+    public static void testOne()
+    {
+        Scanner sc = new Scanner(System.in);
+        int firstInput = sc.nextInt();
+        int secondInput = sc.nextInt();
+        System.out.println(firstInput + secondInput);
+    }
 
 }
