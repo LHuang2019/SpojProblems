840cee4e6793a401129c944413bf0593f89687fd
LHuang2019 <lhuang210@gmail.com>
2016-12-22 22:55:18 -0500

    Solved Character Patterns Act 3

--- src/spojProblems/BasicsProblems.java
+++ src/spojProblems/BasicsProblems.java
@@ -21,6 +21,7 @@ public class BasicsProblems {
         int firstInput = sc.nextInt();
         int secondInput = sc.nextInt();
         System.out.println(firstInput + secondInput);
+        sc.close();
     }
 
     /**
@@ -39,6 +40,7 @@ public class BasicsProblems {
             System.out.println(output);
             output = "";
         }
+        sc.close();
     }
 
     /**
@@ -102,6 +104,39 @@ public class BasicsProblems {
                     character = (i != 0 && i != array[k][0] - 1 && j != 0 && j != array[k][1] - 1) ? '.' : '*';
                     System.out.print(character);
                 }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    
+    /**
+     * Method for solving Character Patterns Act 3
+     * @throws NumberFormatException
+     * @throws IOException
+     */
+    public static void characterPatternsActThree() throws NumberFormatException, IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int numOfInput = Integer.parseInt(reader.readLine());
+
+        int[][] array = new int[numOfInput][2];
+
+        for (int x = 0; x < array.length; x++) {
+            String[] placeHolder = reader.readLine().split(" ");
+            array[x][0] = placeHolder[0] != null ? Integer.parseInt(placeHolder[0]) * 3 + 1 : 0;
+            array[x][1] = placeHolder[1] != null ? Integer.parseInt(placeHolder[1]) : 0;
+        }
+
+        String character = "*";
+
+        for (int k = 0; k < array.length; k++) {
+            for (int i = 0; i < array[k][0]; i++) {
+                for (int j = 0; j < array[k][1]; j++) {
+                    character = (i % 3 == 0) ? "***" : "*..";
+                    System.out.print(character);
+                }
+                System.out.print("*");
                 System.out.println();
             }
             System.out.println();
