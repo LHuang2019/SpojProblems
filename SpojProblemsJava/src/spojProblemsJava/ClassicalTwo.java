49a223763c78d5fc48f026f85022e361ee8d7ca9
Li Huang <lhuang210@gmail.com>
2017-06-17 19:28:09 -0400

    Remove Java solution attempt for classical problem ID 11063, AP - Complete The Series (Easy)

--- SpojProblemsJava/src/spojProblemsJava/ClassicalTwo.java
+++ SpojProblemsJava/src/spojProblemsJava/ClassicalTwo.java
@@ -20,37 +20,6 @@ import spojProblemsJava.FastIO.FastReader;
 public class ClassicalTwo {
 
 	/**
-	 * Attempt classical solution -> Complete The Series (Easy), ID 11063 due to TIME LIMIT EXCEEDED
-	 * @throws NumberFormatException
-	 * @throws IOException
-	 */
-	public static void completeTheSeriesEasy () throws NumberFormatException, IOException
-	{	
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		int numInput = Integer.parseInt(reader.readLine());
-
-		while (numInput-- > 0)
-		{
-			StringTokenizer input = new StringTokenizer(reader.readLine());
-
-			long f = Long.parseLong(input.nextToken());
-			long l = Long.parseLong(input.nextToken());
-			long sum = Long.parseLong(input.nextToken());
-
-			long n = 2 * sum / (f + l);
-			long d = (l - f) / (n - 5);
-			long i = f - 3 * d;
-
-			System.out.println(n);
-
-			while (n-- > 0)
-				System.out.print((i += d) + " ");
-
-			System.out.println();
-		}
-	}
-
-	/**
 	 * Solved classical -> Street Parade, ID 95
 	 * @throws NumberFormatException
 	 * @throws IOException
